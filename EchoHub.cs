d40a2693751fbde1b1318020cec6a8c9326f6147
Antonio Lopac <lopac.antonio@outlook.com>
2017-04-01 19:50:56 +0200

    obrisao i dodao Listener

---
+++ EchoHub.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace HttpListenerTest
+{
+    public class HttpRequestRecievedEventArgs : EventArgs
+    {
+        public string Url { get; set; }
+    }
+
+    public class EchoHub
+    {
+        public event EventHandler HttpRequestRecieved;
+        public string Message { get; set; }
+        public EchoHub(string baseUrl)
+        {
+            var thread = new Thread(() => this.StartServer(baseUrl));
+            thread.Start();
+        }
+
+        private void StartServer(string baseUrl)
+        {
+            var listener = new HttpListener();
+
+            listener.Prefixes.Add(baseUrl);
+
+            try
+            {
+                listener.Start();
+            }
+
+            catch (HttpListenerException hlex)
+            {
+                return;
+            }
+
+
+            while (listener.IsListening)
+            {
+                var context = listener.GetContext();
+                ProcessRequest(context);
+            }
+
+
+            listener.Close();
+        }
+
+
+        private void ProcessRequest(HttpListenerContext context)
+        {
+            // Get the data from the HTTP stream
+            //var body = new StreamReader(context.Request.InputStream).ReadToEnd();
+            OnHttpRequestRecieved(new HttpRequestRecievedEventArgs
+            {
+                Url = context.Request.RawUrl
+            });
+
+            var body = Encoding.UTF8.GetBytes(Message);
+
+            context.Response.StatusCode = 200;
+            context.Response.KeepAlive = true;
+
+
+            context.Response.ContentLength64 = body.Length;
+            context.Response.ContentEncoding = Encoding.UTF8;
+            context.Response.ContentType = "text/html";
+
+
+
+            var output = context.Response.OutputStream;
+
+
+            output.Write(body, 0, body.Length);
+
+
+            context.Response.Close();
+        }
+
+        protected virtual void OnHttpRequestRecieved(HttpRequestRecievedEventArgs e)
+        {
+            var handler = HttpRequestRecieved;
+
+            handler?.Invoke(this, e);
+        }
+    }
+}
---
+++ UsageExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HttpListenerTest
+
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var hub = new EchoHub(baseUrl: "http://+:8080/");
+
+            hub.HttpRequestRecieved += (sender, eventArgs) =>
+            {
+                var data = (HttpRequestRecievedEventArgs) eventArgs;
+
+
+                Console.WriteLine(data.Url);
+
+                //what alexa will says
+                hub.Message = DateTime.Now.ToString("HH:mm:ss");
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
